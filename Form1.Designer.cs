55a07acc246fa8cc91a821a2f5a0d7550e483a53
TishkaaPlay <tikhon42otmyanin@gmail.com>
2025-12-05 03:09:55 +0300

    end

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,12 +29,114 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.buttonClick = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.labelClicks = new System.Windows.Forms.Label();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // buttonClick
+            // 
+            this.buttonClick.BackColor = System.Drawing.Color.White;
+            this.buttonClick.BackgroundImage = global::isrpo_12laba_dz.Properties.Resources.click;
+            this.buttonClick.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonClick.Location = new System.Drawing.Point(12, 89);
+            this.buttonClick.Name = "buttonClick";
+            this.buttonClick.Size = new System.Drawing.Size(199, 112);
+            this.buttonClick.TabIndex = 0;
+            this.buttonClick.UseVisualStyleBackColor = false;
+            this.buttonClick.Click += new System.EventHandler(this.buttonClick_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStart.Location = new System.Drawing.Point(239, 89);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(107, 33);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = false;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Font = new System.Drawing.Font("Impact", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelTime.Location = new System.Drawing.Point(12, 9);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(83, 22);
+            this.labelTime.TabIndex = 2;
+            this.labelTime.Text = "Время: 10";
+            // 
+            // labelClicks
+            // 
+            this.labelClicks.AutoSize = true;
+            this.labelClicks.Font = new System.Drawing.Font("Impact", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelClicks.Location = new System.Drawing.Point(12, 40);
+            this.labelClicks.Name = "labelClicks";
+            this.labelClicks.Size = new System.Drawing.Size(96, 22);
+            this.labelClicks.TabIndex = 3;
+            this.labelClicks.Text = "Нажатий: 0";
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.buttonStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStop.Location = new System.Drawing.Point(239, 128);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(107, 33);
+            this.buttonStop.TabIndex = 4;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = false;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.buttonReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonReset.Location = new System.Drawing.Point(239, 166);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(107, 33);
+            this.buttonReset.TabIndex = 5;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = false;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(384, 211);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.labelClicks);
+            this.Controls.Add(this.labelTime);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.buttonClick);
+            this.Name = "Form1";
+            this.Text = "Кликер";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonClick;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.Label labelClicks;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,70 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace isrpo_12laba_dz
 {
     public partial class Form1 : Form
     {
+        private int time = 10;
+        private int clicks = 0;
+        private bool isActive = false;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            time = 10;
+            clicks = 0;
+            isActive = true;
+
+            labelTime.Text = $"Время: {time}";
+            labelClicks.Text = $"Нажатий: {clicks}";
+
+            timer1.Interval = 1000;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            time--;
+            labelTime.Text = $"Время: {time}";
+
+            if (time <= 0)
+            {
+                timer1.Stop();
+                isActive = false;
+
+                MessageBox.Show($"Конец игры! Твои нажатия: {clicks}");
+                clicks = 0;
+                labelClicks.Text = $"Нажатий: {clicks}";
+            }
+        }
+
+        private void buttonClick_Click(object sender, EventArgs e)
+        {
+            if (isActive == true)
+            {
+                clicks++;
+                labelClicks.Text = $"Нажатий: {clicks}";
+            }
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            time = 0;
+            clicks = 0;
+            isActive = false;
+
+            labelTime.Text = $"Время: {time}";
+            labelClicks.Text = $"Нажатий: {clicks}";
         }
     }
 }
